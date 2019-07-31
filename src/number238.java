881e80a844dec791b2c0108ad6a2a9c37f0ee9d4
KarlWong <wangpan233@gmail.com>
2019-07-31 09:48:12 +0800

    #238

---
+++ src/number238.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class number238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int k = 1;
+        for(int i = 0;i < nums.length;++i){
+            res[i] = k;
+            k *= nums[i];
+        }
+        k = 1;
+        for(int i = res.length - 1; i >= 0;--i){
+            res[i] *= k;
+            k *= nums[i];
+        }
+        return res;
+    }
+}
