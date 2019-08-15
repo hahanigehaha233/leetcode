14e991da4b5adb0ac8a450ec1ebac79301607aff
Wang Pan 612 <wangpan233@gmail.com>
2019-08-15 09:43:05 +0800

    #23

--- src/number78.java
+++ src/number78.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class number78 {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> ans = new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
         res.add(new ArrayList<>());
-        for(int i = 0;i < nums.length;++i){
+        for(int  i = 0;i < nums.length;++i){
             int k = res.size();
             for(int j = 0;j < k;++j){
-                List<Integer> newSub = new ArrayList<Integer>(res.get(j));
-                newSub.add(nums[i]);
-                res.add(newSub);
+                ans = new ArrayList<>(res.get(j));
+                ans.add(nums[i]);
+                res.add(ans);
             }
         }
         return res;
