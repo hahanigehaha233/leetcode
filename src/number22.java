9bf8704fbb333f528286e8ce868980cda1f19633
Wang Pan 612 <wangpan233@gmail.com>
2019-08-19 10:25:12 +0800

    #22 第二次

--- src/number22.java
+++ src/number22.java
@@ -1,25 +1,42 @@
-package com.company;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class number22 {
-    StringBuilder t;
+//    StringBuilder t;
+//    public List<String> generateParenthesis(int n) {
+//        List<String> res = new ArrayList<>();
+//        backtrack(res,"",0,0,n);
+//        return res;
+//    }
+//    public void backtrack(List<String> res,String ans,int l,int r,int n){
+//        if(l + r == 2*n){
+//            res.add(ans);
+//            return;
+//        }
+//        if(l < n){
+//            backtrack(res,ans+'(',l+1,r,n);
+//        }
+//        if(r < l){
+//            backtrack(res,ans+')',l,r+1,n);
+//        }
+//    }
+    List<String> res;
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<>();
-        backtrack(res,"",0,0,n);
+        res = new ArrayList<>();
+        backtrack(n,0,0,"");
         return res;
     }
-    public void backtrack(List<String> res,String ans,int l,int r,int n){
-        if(l + r == 2*n){
-            res.add(ans);
+    public void backtrack(int n,int l,int r,String s){
+        if(n == r){
+            res.add(new String(s));
             return;
         }
         if(l < n){
-            backtrack(res,ans+'(',l+1,r,n);
-        }
-        if(r < l){
-            backtrack(res,ans+')',l,r+1,n);
+            backtrack(n,l+1,r,s+'(');
+        }if(r < l){
+            backtrack(n,l,r+1,s+')');
         }
     }
 }
