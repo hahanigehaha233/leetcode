4b8a0c901e5bbf385b269e13c56900b16c7dc325
KarlWong <wangpan233@gmail.com>
2019-09-17 10:45:38 +0800

    #876

---
+++ src/number876.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class number876 {
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast.next != null){
+            return slow.next;
+        }
+        else{
+            return slow;
+        }
+    }
+}
