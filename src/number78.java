f313066576a71160bf5924cebb60480e64d57083
KarlWong <wangpan233@gmail.com>
2019-09-18 09:27:55 +0800

    again #78

--- src/number78.java
+++ src/number78.java
@@ -1,19 +1,22 @@
+package com.company;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class number78 {
-    List<List<Integer>> res = new ArrayList<>();
-    List<Integer> ans = new ArrayList<>();
-    public List<List<Integer>> subsets(int[] nums) {
-        res.add(new ArrayList<>());
-        for(int  i = 0;i < nums.length;++i){
-            int k = res.size();
-            for(int j = 0;j < k;++j){
-                ans = new ArrayList<>(res.get(j));
-                ans.add(nums[i]);
-                res.add(ans);
-            }
+    List<List<Integer>> res;
+    public void backtrack(int i,List<Integer> tmp,int[] nums){
+        res.add(new ArrayList<>(tmp));
+        for(int j = i;j < nums.length;++j){
+            tmp.add(nums[j]);
+            backtrack(j+1,tmp,nums);
+            tmp.remove(tmp.size() - 1);
         }
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
+        backtrack(0,tmp,nums);
         return res;
     }
 }
